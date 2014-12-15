cdc5c8eb44e4dc03ff94523e2ea774378fe244d2
Travis Cunningham <travcunn@iupui.edu>
2014-12-15 10:19:48 -0500

    Added pyhashxx as a hashing function of the key generator.

--- memcachedkeys/__init__.py
+++ memcachedkeys/__init__.py
@@ -1,15 +1,18 @@
-from memcachedkeys.pkgmeta import *
 import re
 from hashlib import md5
+
+from pyhashxx import hashxx
+
+from memcachedkeys.pkgmeta import *
 
 
 bad_key_chars = re.compile(ur'[\u0000-\u001f\s]+')
 MAX_LENGTH = 250
 
 
-def make_key(key, key_prefix, version):
+def make_key_md5(key, key_prefix, version):
     """
-    Makes a memcached-safe cache key. Use as a KEY_FUNCTION
+    Makes a memcached-safe cache key using md5. Use as a KEY_FUNCTION
 
     """
     clean_key = bad_key_chars.sub('', key)
@@ -21,5 +24,22 @@ def make_key(key, key_prefix, version):
         abbrev_keylen = MAX_LENGTH - len(hashed_key) - len('::[]') - len(key_prefix) - len(version_str)
         new_key = '%s[%s]' % (clean_key[:abbrev_keylen], hashed_key)
         full_key = ':'.join([key_prefix, version_str, new_key])
+
+    return full_key
+
+
+def make_key(key, key_prefix, version):
+    """
+    Makes a memcached-safe cache key using pyhashxx. Use as a KEY_FUNCTION
+
+    """
+    clean_key = bad_key_chars.sub('', key)
+    full_key = '%s:%s:%s' % (key_prefix, version, clean_key)
+
+    if clean_key != key or len(full_key) > MAX_LENGTH:
+        hashed_key = str(hashxx(key))
+        abbrev_keylen = MAX_LENGTH - len(hashed_key) - 4 - len(key_prefix) - len(str(version))
+        new_key = '%s[%s]' % (clean_key[:abbrev_keylen], hashed_key)
+        full_key = '%s:%s:%s' % (key_prefix, version, new_key)
 
     return full_key
--- tests/test_memcachedkeys.py
+++ tests/test_memcachedkeys.py
@@ -2,15 +2,20 @@ from memcachedkeys import make_key
 
 
 def test_short():
+    """ Test short keys. These keys don't require sanitizing. """
     key = 'A' * 241
     full_key = 'prefix:1:%s' % key
     assert full_key == make_key(key, 'prefix', 1)
 
+
 def test_long():
+    """ Test long keys, which will need to be sanitized. """
     key = 'A' * 242
-    hashed_key = '%s[865768d1a3bbc7e33c749dc133260242]' % ('A' * 207)
+    hashed_key = '%s[3705915182]' % ('A' * 229)
     full_key = 'prefix:1:%s' % hashed_key
     assert full_key == make_key(key, 'prefix', 1)
 
+
 def test_space():
-    assert make_key('hello world', 'prefix', '1') == 'prefix:1:helloworld[5eb63bbbe01eeed093cb22bb8f5acdc3]'
+    """ Test for spaces in keys, which will need to be sanitized. """
+    assert make_key('hello world', 'prefix', '1') == 'prefix:1:helloworld[3468387874]'
